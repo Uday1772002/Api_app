bdb98e92375d061d5d5d8ed444bb9061b9cdf753
Jayaram Uday <jayaramuday@Jayarams-MacBook-Air.local>
2023-05-18 15:33:52 +0530

    first commit

---
+++ lib/Models/post.dart
@@ -0,0 +1,57 @@
+// class Post {
+//   int? userId;
+//   int? id;
+//   String? title;
+//   String? body;
+
+//   Post({this.userId, this.id, this.title, this.body});
+
+//   postFromJson(Map<String, dynamic> json) {
+//     userId = json['userId'];
+//     id = json['id'];
+//     title = json['title'];
+//     body = json['body'];
+//   }
+
+//   Map<String, dynamic> toJson() {
+//     final Map<String, dynamic> data = new Map<String, dynamic>();
+//     data['userId'] = userId;
+//     data['id'] = id;
+//     data['title'] = title;
+//     data['body'] = body;
+//     return data;
+//   }
+// }
+import 'dart:convert';
+
+List<Post> postFromJson(String str) => List<Post>.from(json.decode(str).map((x) => Post.fromJson(x)));
+
+String postToJson(List<Post> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Post {
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    this.body,
+  });
+
+  int userId;
+  int id;
+  String title;
+  String? body;
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        body: json["body"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "body": body,
+      };
+}
